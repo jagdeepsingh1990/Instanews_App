5517e7354945bae26a3aecd1477d5e07fa2781ac
Jagdeep <simran6233@gmail.com>
2018-11-07 21:01:22 -0800

    automation_done

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,30 @@
 var gulp = require("gulp"), // Load Gulp!
-// Now that we've installed the uglify package we can require it:
- uglify = require("gulp-uglify"),
- eslint = require("gulp-eslint"),
+  // Now that we've installed the uglify package we can require it:
+  uglify = require("gulp-uglify"),
+  eslint = require("gulp-eslint"),
   rename = require("gulp-rename"),
   sass = require("gulp-sass"),
   autoprefixer = require("gulp-autoprefixer"),
   cssnano = require("gulp-cssnano"),
-  prettyError = require('gulp-prettyerror');
+  prettyError = require("gulp-prettyerror");
 
-  var browserSync = require('browser-sync').create();
+var browserSync = require("browser-sync").create();
 
-gulp.task('script', function() {
-  return gulp.src('./js/*.js') // What files do we want gulp to consume?
+gulp.task("script", function() {
+  return gulp
+    .src("./js/*.js") // What files do we want gulp to consume?
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(uglify()) // Call the uglify function on these files
     .pipe(rename({ extname: ".min.js" })) // Rename the uglified file
-    .pipe(gulp.dest("./build/js")); // Where do we put the result?
-
-
-    
+    .pipe(gulp.dest("./build/js")) // Where do we put the result?
+    .pipe(browserSync.stream());
 });
 
 gulp.task("sass", function() {
   return gulp
     .src("./sass/style.scss")
-    .pipe(prettyError()) 
+    .pipe(prettyError())
     .pipe(sass())
     .pipe(
       autoprefixer({
@@ -35,21 +34,27 @@ gulp.task("sass", function() {
     .pipe(gulp.dest("./build/css"))
     .pipe(cssnano())
     .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("./build/css"));
+    .pipe(gulp.dest("./build/css"))
+    .pipe(browserSync.stream());
 });
 
-gulp.task("print_Hello",function(hello){
-  console.log("Hi Its gulp")
-  hello();
-})
 
-gulp.task('browser-sync', function() {
+
+gulp.task("watch", function() {
+  gulp.watch(["./sass/*.scss"], gulp.series("sass")); // Reload on vendorsJs file changes.
+  gulp.watch("./js/*.js", gulp.series("script"));
+ 
+});
+
+gulp.task("browser-sync", function() {
   browserSync.init({
-      server: {
-          baseDir: "./"
-      }
+    open: false,
+    injectChanges:true,
+    server: {
+      baseDir: "./"
+    }
   });
-  gulp.watch(['build/js/*.js']).on('chnage',browserSync.reload);
+  gulp.watch(["./build/js/*.js", "./build/css/*.css"]).on("change", browserSync.reload);
 });
 
-gulp.task("default", gulp.series( "script",'sass', 'browser-sync'));
+gulp.task("default", gulp.parallel( "browser-sync","watch"));
--- js/main.js
+++ js/main.js
@@ -2,67 +2,128 @@ var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
 
 var data = [];
 var sectionDataArray = [];
-url +=
-  "?" +
-  $.param({
-    'api-key': "cec89a9c54f24354af812aedc5a43321"
-  });
-  console.log(url)
+var uniqueValues = [];
+
+
+option value="politics">U.S.</option>
+         <option value="world">World</option>
+         <option value="national">National</option>
+         <option value="upshot">Upshot</option>
+         <option value="nyregion">NY Region</option>
+         <option value="business">Business</option>
+         <option value="technology">samosa</option>
+         <option value="science">Science</option>
+         <option value="health">Health</option>
+         <option value="opinion">Opinion</option>
+         <option value="sports">Sports</option>
+         <option value="arts">Arts</option>
+         <option value="books">Books</option>
+         <option value="movies">Movies</option>
+         <option value="theater">Theather</option>
+         <option value="sundayreview">Sunday Review</option>
+         <option value="fashion">Fashion</option>
+         <option value="tmagazine">Tmagazine</option>
+         <option value="food">Food</option>
+         <option value="travel">Travel</option>
+         <option value="magazine">Magazine</option>
+         <option value="realestate">Real Estate</option>
+         <option value="automobiles">Auto Mobiles</option>
+         <option value="obituaries">Obituaries</option>
+         <option value="insider">Insider</option>
+Message Input
+
+
 $.ajax({
-  url: url,
+  url: addApi_key(url),
   method: "GET"
-  
-}).done(function(result) {
-     data = result.results;
-    console.log(result);
-     sectionDataArray = data.map(function(value) {
+})
+  .done(function(result) {
+    data = result.results;
+    console.log(data)
+    sectionDataArray = data.map(function(value) {
       return value.section;
     });
-    console.log(sectionDataArray);
-    
-    //Remove duplicate value from Array
-    function removeDups(names) {
-      var unique = {};
-      names.forEach(function(i) {
-        if (!unique[i]) {
-          unique[i] = true;
-        }
-      });
-      return Object.keys(unique);
-    }
-    var uni = removeDups(sectionDataArray);
-    console.log(uni);
-
-    // $('#select_options').append($('<option>', {
-    //     value: 1,
-    //     text: 'My option'
-    // }));
-    $('#select_options').empty();
-    $.each(uni, function(key, value) {
-      $('#select_options').append(
-        $('<option/>', {
-          value: value,
-          text: value
-        })
-      );
-    });
-    $('#select_options').change(function() {
-        var val = $('#select_options option:selected').text().replace(" ", "");
-        console.log(val);
-        $.each( data, function( index, value ){
-          if (val == value.section){
-            console.log(value.section)
-            // console.log("True")
-          }
-      });
-      });
+    uniqueValues = removeDups(sectionDataArray);
+    addOptionsToList(uniqueValues);
   })
-  
-  
   .fail(function(err) {
     throw err;
   });
 
 
- 
+//Add options to list
+function addOptionsToList(value) {
+  $.each(value, function(key, value) {
+    $("#select_options").append(
+      $("<option/>", {
+        value: value,
+        text: value
+      })
+    );
+  });
+}
 
+//on select option
+$("#select_options").change(function() {
+  var option_text = $("#select_options option:selected")
+    .text()
+    .replace(" ", "");
+  url = "https://api.nytimes.com/svc/topstories/v2/" + option_text + ".json";
+  console.log(addApi_key(url))
+  $.ajax({
+    url: addApi_key(url),
+    method: "GET"
+  })
+    .done(function(result) {
+      data = result;
+      console.log(data)
+    })
+    .fail(function(err) {
+      throw err;
+    });
+
+  addNewsToDiv(data, option_text);
+});
+
+// add apiKey to Url
+function addApi_key(urlText) {
+  urlText +=
+    "?" +
+    $.param({
+      "api-key": "cec89a9c54f24354af812aedc5a43321"
+    });
+  return urlText;
+}
+// Add data to news Conatiner
+function addNewsToDiv(dataArr, val) {
+  $.each(dataArr, function(index, value) {
+    if (val === value.section) {
+      var abstract = value.abstract;
+      var newsUrl = value.url;
+      if (value.multimedia.length !== 0) {
+        var imgurl = value.multimedia[4].url;
+
+        $(".newsData").append(
+          '<div class= "newsDiv"><img src = "' +
+            imgurl +
+            '"><a target = "_blank" href = "' +
+            newsUrl +
+            '" ><p>' +
+            abstract +
+            "</p></a></div>"
+        );
+      }
+    }
+  });
+}
+
+//Remove duplicate value from Array
+function removeDups(names) {
+  var unique = {};
+  names.forEach(function(i) {
+    if (!unique[i]) {
+      unique[i] = true;
+    }
+  });
+  return Object.keys(unique);
+}
